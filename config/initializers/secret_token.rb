5699b95ef8dad3505c8d68ead7cee4ae1469e875
Kosuke Tsunetomo <kosuke.t.friend@gmail.com>
2015-09-06 22:06:13 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FistApp::Application.config.secret_key_base = '85d00781e035f84b9c0146dd046068138dcdc9ee693e851737bfbf3f3a276f9fd1933f7a7c7a1697fb6ae07b7512e454ce5087ee4fc777656065edbabdb373e1'
